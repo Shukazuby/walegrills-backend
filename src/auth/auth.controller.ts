0c5bde745e2d8f0871ca6f8bac589870dca8e3d5
Shukazuby <shukazuby@gmail.com>
2025-05-06 22:01:51 +0100

    added api Tags to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,9 +11,10 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterAdminDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BaseResponseTypeDTO } from 'src/utils';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
